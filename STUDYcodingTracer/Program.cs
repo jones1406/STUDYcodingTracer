7492c8f374d7537237092f9425921b2dde2ecab4
jones1406 <enihsjones@gmail.com>
2026-06-17 17:30:30 +0800

    Keeping one message

--- STUDYcodingTracer/Program.cs
+++ STUDYcodingTracer/Program.cs
@@ -1,5 +1,1 @@
 ﻿Console.WriteLine("Hello, C# World dot Pluss pluss!! One more messages");
-Console.WriteLine("Hello, C# World dot Pluss pluss!! One more messages");
-Console.WriteLine("Hello, C# World dot Pluss pluss!! One more messages");
-Console.WriteLine("Hello, C# World dot Pluss pluss!! One more messages");
-Console.WriteLine("Hello, C# World dot Pluss pluss!! One more messages");
